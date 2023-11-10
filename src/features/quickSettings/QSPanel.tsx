15f98351d853f8f6006c5588bddd7f6421f068ab
Vivien F <vivien@netc.fr>
2023-11-10 22:40:06 +0100

    implemented global activation in QS

--- src/features/quickSettings/QSPanel.tsx
+++ src/features/quickSettings/QSPanel.tsx
@@ -1,10 +1,21 @@
-export default function QSPanel() {
+import {selectEnabled} from "../preferences/settingsSlice";
+import {useState} from "react";
+import {useSelector} from "react-redux";
 
+export default function QSPanel() {
+    let emojiEnabled = useSelector(selectEnabled)
+    const toggleEnabled = () => {
+        // @ts-ignore
+        browser.runtime.sendMessage({
+            type: "toggle-emoji",
+            emoji_active: !emojiEnabled
+        })
+    }
 
     return (
         <>
             <button>Disable for this site</button>
-            <button>Disable extension</button>
+            <button onClick={toggleEnabled}>{emojiEnabled ? "Disable extension" : "Enable extension"}</button>
         </>
     )
 }
