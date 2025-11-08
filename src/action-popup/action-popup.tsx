2e059abb1a9c3f6f50b272710779fd56ad33afa7
Vivien F <vivien@netc.fr>
2025-11-08 18:25:27 +0100

    added status in the control popup

--- src/action-popup/action-popup.tsx
+++ src/action-popup/action-popup.tsx
@@ -35,12 +35,27 @@ function ActionPopup({
     const onSetEnabledForSite = () => {
         onToggleEnabledForSite(!enabledForSite, siteSettings.url);
     }
+    let hostname = ""
+    try {
+        hostname = new URL(siteSettings.url).host
+    } catch (e) {
+        hostname = siteSettings.url;
+    }
+
     console.log(`Re-Render : enabled=${enabled} , enabledForSite=${enabledForSite}\n\tParams : enabled=${siteSettings.enabled}, disabledGlobally=${siteSettings.disabledGlobally}, url=${siteSettings.url}`);
+    const statusText = enabled ? (enabledForSite ? "Active on " : "Disabled for ") + hostname : "Disabled globally"
+    const statusColor = enabled ? enabledForSite ? "green" : "orange" : "red"
 
     return (
         <>
             <button className={enabled ? "" : "outlined"} onClick={onSetEnabled}>{enabled ? "Pause Extension" : "Activate"}</button>
             <button className={enabledForSite ? "": "outlined"} onClick={onSetEnabledForSite} style={{display: siteSettings.disabledGlobally ? "none" : "inline"}}>{enabledForSite ? "Disable" : "Enable"} For This Site</button>
+            <div className={"status"}>
+                <svg style={{width: "10px", height: "10px", marginRight: "5px"}}>
+                    <circle cx="5" cy="5" r="5" fill={statusColor}/>
+                </svg>
+                <p style={{color: statusColor}}>{statusText}</p>
+            </div>
             <footer>
                 <a href={"https://github.com/Choucroute-melba/Emoji2"}>GitHub</a> - <a href="https://github.com/Choucroute-melba/emoji/issues/new">Report a Bug</a> - <a href={"https://addons.mozilla.org/en-US/firefox/addon/emojeezer/"}>Leave a Review</a>
             </footer>
