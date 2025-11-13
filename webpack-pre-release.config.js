d7f69e1bd19e2d78b3ccc268904f90ff064b66be
Vivien F <vivien@netc.fr>
2025-11-13 22:23:12 +0100

    fixed pre-release build script

--- webpack-pre-release.config.js
+++ webpack-pre-release.config.js
@@ -49,7 +49,7 @@ export default {
             firefox: "C:\\Program Files\\Firefox Developer Edition\\firefox.exe",
             firefoxProfile: "C:\\Users\\Vivien\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\kv2tckr8.dev-edition-default",
             buildPackage: true,
-            outputFilename: `emojeezer-v-${version}.zip`,
+            outputFilename: `emojeezer-b-${version}.zip`,
             overwriteDest: true
         }),
         new CopyPlugin({
