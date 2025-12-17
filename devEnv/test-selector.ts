4110c1840a66ebee6c43150fa5a310a80f9d4aea
Vivien F <vivien@netc.fr>
2025-12-17 01:43:18 +0100

    Added a build config for easier ui development

---
+++ devEnv/test-selector.ts
@@ -0,0 +1,105 @@
+import EmojiSelector from "../src/selector/emojiselector";
+import {searchEmoji} from "../src/emoji/emoji";
+import {createRoot} from "react-dom/client";
+import React from "react";
+import EmojiCard from "../src/selector/Components/EmojiCard";
+import Star from "../src/selector/Components/Star";
+
+console.log('test');
+
+const es = new EmojiSelector();
+es.addToDom()
+es.display = true;
+es.onClose = () => console.log("closed")
+es.onEmojiSelected = (emoji) => console.log(emoji)
+es.onBlur = () => console.log("blurred")
+es.onResize = () => console.log("resized")
+
+console.log(es)
+
+const input = document.getElementById("emojiSelectorTest") as HTMLInputElement;
+input.focus();
+const inputRect = input.getBoundingClientRect();
+es.geometry = {
+    position: {
+        x: inputRect.x,
+        y: inputRect.y + inputRect.height + 5,
+    },
+    placement: "down",
+    positionMode: "absolute",
+};
+input.addEventListener('input', (e) => {
+    const value = (e.target as HTMLInputElement).value;
+    const results = searchEmoji(value, [], 40)
+    es.searchResults = results;
+    renderPreviews(false);
+})
+input.addEventListener('keydown', (e) => {
+    if(e.key === "Enter") {
+    }
+    if(e.key === "ArrowDown") {
+        e.preventDefault();
+        e.stopPropagation();
+        es.focusDown()
+        renderPreviews(false);
+    }
+    if(e.key === "ArrowUp") {
+        e.preventDefault();
+        e.stopPropagation();
+        es.focusUp()
+        renderPreviews(false);
+    }
+})
+
+es.searchResults = searchEmoji(input.value, [], 40)
+
+const previewSquare = document.getElementById("previewSquare") as HTMLDivElement;
+const previewFull = document.getElementById("previewFull") as HTMLDivElement;
+const previewSquareSelected = document.getElementById("previewSquareSelected") as HTMLDivElement;
+const previewFullSelected = document.getElementById("previewFullSelected") as HTMLDivElement;
+const starPreview = document.getElementById("star") as HTMLDivElement;
+const filledStarPreview = document.getElementById("filledStar") as HTMLDivElement;
+const rootSquare = createRoot(previewSquare);
+const rootFull = createRoot(previewFull);
+const rootSquareSelected = createRoot(previewSquareSelected);
+const rootFullSelected = createRoot(previewFullSelected);
+const rootStar = createRoot(starPreview);
+const rootFilledStar = createRoot(filledStarPreview);
+
+const renderPreviews = (focused: boolean) => {
+    rootSquare.render(React.createElement(EmojiCard, {
+        emoji: es.getFocusedEmoji(),
+        cardStyle: "square",
+        selected: false,
+        onClick: () => console.log("clicked")
+    }))
+
+    rootFull.render(React.createElement(EmojiCard, {
+        emoji: es.getFocusedEmoji(),
+        cardStyle: "full",
+        selected: false,
+        onClick: () => console.log("clicked")
+    }))
+
+    rootFullSelected.render(React.createElement(EmojiCard, {
+        emoji: es.getFocusedEmoji(),
+        cardStyle: "full",
+        selected: true,
+        onClick: () => console.log("clicked")
+    }))
+
+    rootSquareSelected.render(React.createElement(EmojiCard, {
+        emoji: es.getFocusedEmoji(),
+        cardStyle: "square",
+        selected: true,
+        onClick: () => console.log("clicked")
+    }))
+
+    rootStar.render(React.createElement(Star, {filled: false, width:60, height:60}))
+    rootFilledStar.render(React.createElement(Star, {filled: true, width:60, height:60}))
+}
+
+renderPreviews(false)
+
+previewSquare.addEventListener('mouseenter', () => renderPreviews(true))
+previewSquare.addEventListener('mouseleave', () => renderPreviews(false))
---
+++ webpack-dev-ui.config.js
@@ -0,0 +1,76 @@
+import * as path from "node:path";
+import {fileURLToPath} from "node:url";
+import CopyPlugin from "copy-webpack-plugin";
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: {
+        'test-selector': './devEnv/test-selector.ts'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.module\.css$/i,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /(?<!\.module)\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            '@src': path.resolve(__dirname, 'src'),
+            '@devEnv': path.resolve(__dirname, 'devEnv'),
+        }
+    },
+    output: {
+        filename: '[name]-bundle.js',
+        path: path.resolve(__dirname, 'dev'),
+        clean: false,
+    },
+    devServer: {
+        static: './dev',
+        allowedHosts: "all",
+        host: 'localhost',
+        client: {
+            webSocketURL: {
+                hostname: 'localhost',
+                port: 8080,
+            }
+        },
+        watchFiles: {
+            paths: ['src/**/*', 'assets/**/*', 'devEnv/**/*'],
+            options: {
+                ignored: ['dev/**/*', "extensions/**/*", "node_modules/**/*"],
+            },
+        },
+    },
+    devtool: 'source-map',
+    mode: 'development',
+
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {from: 'assets', to: 'assets'},
+                {from: 'devEnv', to: ''},
+            ]
+        })
+    ]
+}
