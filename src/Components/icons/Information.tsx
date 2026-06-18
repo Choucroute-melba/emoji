afee03ebba3283a083e9e220399dd77e810a0be0
Vivien F <vivien@netc.fr>
2026-06-18 18:13:25 +0200

    Improved clarity of the add-on status in the action popup

---
+++ src/Components/icons/Information.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function InformationIcon() {
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <circle cx="12" cy="12" r="9"></circle>
+            <line x1="12" y1="8" x2="12.01" y2="8"></line>
+            <polyline points="11 12 12 12 12 16 13 16"></polyline>
+        </svg>
+    )
+}
--- src/action-popup/action-popup.tsx
+++ src/action-popup/action-popup.tsx
@@ -5,6 +5,7 @@ import {SiteSettings} from "../background/types";
 import browser from "webextension-polyfill";
 import {getDomainName} from "../background/storage-utils";
 import ToggleButton from "@src/Components/ToggleButton";
+import InformationIcon from "@src/Components/icons/Information";
 
 export default function ActionPopup({
     siteSettings,
@@ -38,20 +39,36 @@ export default function ActionPopup({
     console.log(`Re-Render : enabled=${enabled} , enabledForSite=${enabledForSite}\n\tParams : enabled=${siteSettings.enabled}, disabledGlobally=${!enabledGlobally}, url=${siteSettings.url}`);
     const statusText = enabled ? (enabledForSite ? "Active on " : "Disabled for ") + hostname : "Disabled globally"
     const statusColor = enabled ? enabledForSite ? "var(--fx-success-text)" : "var(--fx-warning-text)" : "var(--fx-critical-text)"
+    const [keyboardShortcut, setKeyboardShortcut] = React.useState<string | null>(null);
+    browser.commands.getAll().then((commands) => {
+        const cmd = commands.find((command) => command.name === "show-free-selector");
+        if(cmd) {
+            setKeyboardShortcut(cmd.shortcut || "null");
+        }
+    })
 
     return (
         <div>
             <div className={"highlightSection " + (enabled ? "enabled" : "disabled")}>
                 <ToggleButton onChange={onSetEnabled} checked={enabled}>{enabled ? "Autocomplete ON" : "Autocomplete OFF"}</ToggleButton>
             </div>
-            <div className={"siteControlSection highlightSection disabled"}>
+            <div className={"siteControlSection highlightSection outlined"}>
                 <div className={"status"}>
-                    <p style={{color: statusColor}}>
-                    <span><svg style={{width: "10px", height: "10px", marginRight: "5px"}}>
-                        <circle cx="5" cy="5" r="5" fill={statusColor}/>
-                    </svg></span>
-                    {statusText}</p>
+                    <div className={"infoBox"}>
+                        <svg style={{width: "10px", height: "10px", marginRight: "5px"}} viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="6" cy="6" r="5" fill={statusColor}/>
+                        </svg>
+                        <p style={{color: statusColor}} className={"info"}>{statusText}</p>
+                    </div>
+                    {
+                        ((!siteSettings.enabled || !enabledGlobally) && keepFreeSelectorEnabled) && <div className={"infoBox"}>
+                            <InformationIcon/>
+                            <p className={"info"}>You can still copy emoji using the {keyboardShortcut} keyboard shortcut. Visit settings to change this.
+                            </p>
+                        </div>
+                    }
                 </div>
+
                 <button className={"siteControlButton " + (enabledForSite ? "outlined" : "accent")} onClick={onSetEnabledForSite} style={{display: !enabledGlobally ? "none" : "block"}}>{enabledForSite ? "Disable" : "Enable"} For This Site</button>
             </div>
             <footer>
